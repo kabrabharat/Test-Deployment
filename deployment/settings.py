21c8d3764bd675d7613469045f85c0582553e577
kabrabharat <bharat.kabra33@gmail.com>
2022-11-14 15:02:27 +0530

    created collect static files

--- deployment/settings.py
+++ deployment/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATIS_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
